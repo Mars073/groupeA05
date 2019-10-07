b2dfd37e038a2370b67ce3192aaecc4cdc5ddb70
mars073 <imars073@live.be>
2019-10-07 14:57:11 +0200

    Comment Game.h

--- pgroupeA05/include/Game.h
+++ pgroupeA05/include/Game.h
@@ -8,21 +8,32 @@ using namespace sf;
 class Game
 {
     private:
-        static Game* instance; // singleton
+        static Game* instance;          //!< Singleton instance
+        /** private constructor */
         Game();
-        Scene* scene = new HomeScene;
-        RenderWindow *window = nullptr;
+        Scene* scene = new HomeScene;   //!< Pointer "Scene"
+        RenderWindow* window = nullptr; //!< Pointer "RenderWindow"
 
     public:
+        /** Access Singleton instance
+         * \return A pointer of the Game instance
+         */
         static Game* getInstance();
+        /** instance destructor */
         virtual ~Game();
 
+        /** Draw all of Game component */
         void draw();
+        /** Access RenderWindow Instance */
         RenderWindow* getWindow();
+        /** Set active Scene
+         * \param ptr new Scene to set
+         */
         void setScene(Scene*);
+        /** Access active Scene */
         Scene* getScene();
 
-        // raccourcis game <-> window
+        // Shortcuts game <-> window
         bool isOpen();
         bool pollEvent(Event&);
         void pollEvent();
