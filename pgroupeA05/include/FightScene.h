5086ae88a9bcc6e58ed752c7cc215918c4e2e016
Mars073 <imars073@live.be>
2019-10-12 23:09:14 +0200

    quick fixe

--- pgroupeA05/include/FightScene.h
+++ pgroupeA05/include/FightScene.h
@@ -1,15 +1,23 @@
-#ifndef FIGHTSCENE_H
-#define FIGHTSCENE_H
+#ifndef FightScene_H
+#define FightScene_H
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include "WindowsFight.h"
+#include "vector"
+#include "iostream"
 
 
 class FightScene
 {
     public:
         FightScene();
+        virtual ~FightScene();
+        void fight();
 
     protected:
 
     private:
 };
 
-#endif // FIGHTSCENE_H
+#endif // FightScene_H
