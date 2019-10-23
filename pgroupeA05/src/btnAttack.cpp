affbcd43158b96b4d8baed239ab6683ad3824cac
Nevo067 <51850812+Nevo067@users.noreply.github.com>
2019-10-23 11:41:28 +0200

    Update btnAttack.cpp

--- pgroupeA05/src/btnAttack.cpp
+++ pgroupeA05/src/btnAttack.cpp
@@ -7,6 +7,7 @@ btnAttack::btnAttack(int positionX,int positionY,int tailleX,int tailleY,std::st
 void btnAttack::action(Player *pl,Monster *ms)
 {
     std::cout << "vous attaquez" << std::endl;
+
 }
 
 btnAttack::~btnAttack()
