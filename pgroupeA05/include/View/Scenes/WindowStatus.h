9df7d430bd107a1dfe14488c90e112d9485b6cd7
TigrouSimba <56092005+TigrouSimba@users.noreply.github.com>
2019-10-28 16:54:40 +0100

    forgot to delete wrong include

--- pgroupeA05/include/View/Scenes/WindowStatus.h
+++ pgroupeA05/include/View/Scenes/WindowStatus.h
@@ -7,7 +7,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
-#include <View/Scenes/WindowEquipment.h>
 using namespace sf;
 using namespace std;
 class WindowStatus: public StrategyScene
