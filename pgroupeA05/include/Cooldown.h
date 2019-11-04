036c4013e009cad90f82ec606b069dfda17b7feb
Nevo067 <51850812+Nevo067@users.noreply.github.com>
2019-11-04 10:09:52 +0100

    add Cooldown Class

---
+++ pgroupeA05/include/Cooldown.h
@@ -0,0 +1,32 @@
+#ifndef COOLDOWN_H
+#define COOLDOWN_H
+
+#include <SFML/System.hpp>
+
+
+class Cooldown
+{
+    public:
+        /** Default constructor */
+        Cooldown();
+        /** Default destructor */
+        virtual ~Cooldown();
+        /** Copy constructor
+         *  \param other Object to copy from
+         */
+        Cooldown(const Cooldown& other);
+        /** Assignment operator
+         *  \param other Object to assign from
+         *  \return A reference to this
+         */
+        Cooldown& operator=(const Cooldown& other);
+        static bool isTimePassed(float &valActuel,float maxVal);
+
+    protected:
+
+    private:
+        sf::Clock clock;
+        sf::Time elapsed;
+};
+
+#endif // COOLDOWN_H
