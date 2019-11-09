6c44b55e3138f51e92f53dbf6a90ba0bdaa27015
Mars073 <imars073@live.be>
2019-11-09 23:50:06 +0100

    Add (empty credits scene)

---
+++ pgroupeA05/include/View/Scenes/ConcreteStrategyCreditsScene.h
@@ -0,0 +1,19 @@
+#ifndef VIEW_SCENES_CONCRETESTRATEGYCREDITSSCENE_H
+#define VIEW_SCENES_CONCRETESTRATEGYCREDITSSCENE_H
+
+#include <View/StrategyScene.h>
+
+
+class ConcreteStrategyCreditsScene : public StrategyScene
+{
+    private:
+
+    public:
+        ConcreteStrategyCreditsScene();
+        virtual ~ConcreteStrategyCreditsScene();
+        void draw(RenderTarget&, RenderStates) const override;
+        void eventHandler(Event) override;
+
+};
+
+#endif // VIEW_SCENES_CONCRETESTRATEGYCREDITSSCENE_H
---
+++ pgroupeA05/src/View/Scenes/ConcreteStrategyCreditsScene.cpp
@@ -0,0 +1,20 @@
+#include "View\Scenes\ConcreteStrategyCreditsScene.h"
+
+ConcreteStrategyCreditsScene::ConcreteStrategyCreditsScene()
+{
+    //ctor
+}
+
+ConcreteStrategyCreditsScene::~ConcreteStrategyCreditsScene()
+{
+    //dtor
+}
+
+void ConcreteStrategyCreditsScene::draw(RenderTarget& target, RenderStates states) const
+{
+
+}
+void ConcreteStrategyCreditsScene::eventHandler(Event event)
+{
+
+}
