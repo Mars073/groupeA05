801026ad37a33c4138d469e97fee7ca21c417437
Mars073 <imars073@live.be>
2019-11-10 20:00:22 +0100

    Add FightBG & Boss in the tex loader

--- pgroupeA05/include/View/Scenes/ConcreteStrategyLoadScreenScene.h
+++ pgroupeA05/include/View/Scenes/ConcreteStrategyLoadScreenScene.h
@@ -16,20 +16,22 @@ class ConcreteStrategyLoadScreenScene: public StrategyScene
 {
     private:
         int loaded = 0;
-        const int MAX_LOAD = 13;
+        const int MAX_LOAD = 15;
         string error;
         string fonts[2][2] = {
             { "arial", "data/fonts/arial.ttf"},
             { "morpheus", "data/fonts/morpheus.ttf"}
         };
-        string images[7][2] = {
+        string images[9][2] = {
             {"icon", "data/images/icon.png"},
             {"title", "data/images/background-title.png"},
+            {"fight", "data/images/BackGroudCombat.png"},
             {"character", "data/images/character_sprite.png"},
             {"map", "data/images/simplemap_sprite.png"},
             {"mob_goblin", "data/images/monsters/goblin.png"},
             {"mob_skeleton", "data/images/monsters/skeleton.png"},
-            {"mob_wolf", "data/images/monsters/wolf.png"}
+            {"mob_wolf", "data/images/monsters/wolf.png"},
+            {"mob_boss", "data/images/monsters/boss.png"}
         };
         string sounds[4][2] = {
             {"title", "data/sounds/title.ogg"},
