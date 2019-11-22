51c0b9ac05bc30c463cc4478319c1a39b9073403
Mars073 <imars073@live.be>
2019-11-22 17:17:35 +0100

    Remove comments

--- pgroupeA05/main.cpp
+++ pgroupeA05/main.cpp
@@ -1,11 +1,9 @@
 #include "Model/SingletonGame.h"
-//#include "FightScene.h"
 
 int main()
 {
-    //FightScene *fn = new FightScene();
     SingletonGame* game = SingletonGame::getInstance();
-    //game->setScene(fn);
+
     while (game->isOpen())
     {
         game->pollEvent();
